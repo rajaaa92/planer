d718737ae9579d76b27ede5ebbeb4c4fe12b21ee
Joanna Rajewska <contact@rajewska.asia>
2016-02-19 21:22:19 +0100

    try to fix js on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,10 @@ Planer::Application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
+  config.assets.precompile += %w( *.js *.css )
   config.assets.digest = true
   config.assets.version = '1.0'
   config.log_level = :info
